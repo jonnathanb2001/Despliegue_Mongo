541f9b1be0c7472e82b2795305555c7ea5121df8
jonnathanb2001 <buenopaul111@gmail.com>
2023-07-07 21:20:05 -0500

    Proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 import './App.css';
+import ProductContextProvider from './contexts/ProductContext';
+import ProductList from './components/ProductList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ProductContextProvider>
+            <ProductList/>
+        </ProductContextProvider>
     </div>
   );
 }
